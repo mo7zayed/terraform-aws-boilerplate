2a6641dc9ce1d4b81c34f3a67bc7ea2c8c4fcde7
mo7zayed <mohamedzayed907@yahoo.com>
2023-07-17 14:13:40 +0300

    Added ec2/bastion module

---
+++ modules/ec2/bastion/main.tf
@@ -0,0 +1,52 @@
+resource "aws_security_group" "bastion_sg" {
+  name        = "bastion-server-sg"
+  description = "Allow incoming SSH connections"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow incoming SSH connections"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Tags
+  tags = merge(
+    {
+      Env = var.env
+    },
+    var.global_tags
+  )
+}
+
+module "bastion-server" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+
+  depends_on = [aws_security_group.bastion_sg]
+
+
+  name                        = "${var.project}-${var.env}-bastion-server"
+  ami                         = data.aws_ami.ubuntu_22_04_ami.id
+  instance_type               = var.bastion_instance_type
+  subnet_id                   = var.subnet_id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
+  #   key_name                    = "mo7zayed-root-us-east-1"
+  #   monitoring                  = true
+
+  # Tags
+  tags = merge(
+    {
+      Env = var.env
+    },
+    var.global_tags
+  )
+}
